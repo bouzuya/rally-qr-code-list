46ca35f666d68957a8ede6561de254827cb8416b
bouzuya <m@bouzuya.net>
2018-08-26 12:38:10 +0900

    add SignInForm to Home

--- src/Share/View/Home.purs
+++ src/Share/View/Home.purs
@@ -1,30 +1,33 @@
 module Share.View.Home
   (view) where
 
-import Prelude (Unit)
+import Prelude (Unit, discard, ($))
 import Pux.DOM.HTML as P
 import Share.State (State)
+import Share.View.SignInForm as SignInForm
 import Text.Smolder.HTML as H
 import Text.Smolder.HTML.Attributes as HA
 import Text.Smolder.Markup as M
 
 view :: State -> P.HTML Unit
 view state = do
-  H.html do
-    H.head do
+  H.html $ do
+    H.head $ do
       H.meta M.! HA.charset "UTF-8"
-      H.title "RALLY QR code list"
-    H.body do
-      H.header do
-        H.h1 do
+      H.title $ do
+        M.text "RALLY QR code list"
+    H.body $ do
+      H.header $ do
+        H.h1 $ do
           M.text "RALLY QR code list"
-      H.div M.! HA.className "body" do
-        H.p do
+      H.div M.! HA.className "body" $ do
+        H.p $ do
           M.text "Hello!"
-      H.footer do
-        H.div M.! HA.className "source-code" do
-          H.a M.! HA.href "https://github.com/bouzuya/rally-qr-code-list" do
+        SignInForm.view state
+      H.footer $ do
+        H.div M.! HA.className "source-code" $ do
+          H.a M.! HA.href "https://github.com/bouzuya/rally-qr-code-list" $ do
             M.text "Source Code"
-        H.address do
-          H.a M.! HA.href "https://bouzuya.net" do
+        H.address $ do
+          H.a M.! HA.href "https://bouzuya.net" $ do
             M.text "bouzuya"
