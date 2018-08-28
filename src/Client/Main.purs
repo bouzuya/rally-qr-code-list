3db41df0b44251bc690098d8b8c49ef7d6358ca7
bouzuya <m@bouzuya.net>
2018-08-28 23:46:26 +0900

    add client entry point

--- src/Client/Main.purs
+++ src/Client/Main.purs
@@ -3,7 +3,24 @@ module Client.Main
 
 import Effect (Effect)
 import Effect.Class.Console (log)
-import Prelude (Unit)
+import Prelude (Unit, bind, discard)
+import Pux as Pux
+import Pux.Renderer.React as PR
+import Share.Event (foldp)
+import Share.View.ClientRoot as ClientRoot
 
 main :: Effect Unit
-main = log "client"
+main = do
+  log "client"
+  let
+    puxConfig =
+      { initialState:
+        { email: "email@example.com"
+        , password: "pass1"
+        }
+      , view: ClientRoot.view
+      , foldp
+      , inputs: []
+      }
+  app <- Pux.start puxConfig
+  PR.renderToDOM ".client-root" app.markup app.input
