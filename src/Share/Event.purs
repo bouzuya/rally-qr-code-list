cfa171608ad62d9d657d71c48a699475578b5712
bouzuya <m@bouzuya.net>
2018-08-30 23:01:19 +0900

    add SignInSuccess event

--- src/Share/Event.purs
+++ src/Share/Event.purs
@@ -1,27 +1,38 @@
 module Share.Event
   ( Event(..)
+  , RallyToken
   , foldp
   ) where
 
 import Bouzuya.HTTP.Client (body, fetch, headers, method, url)
 import Bouzuya.HTTP.Method as Method
+import Data.Either (either)
 import Data.Maybe (Maybe(..))
 import Data.Options ((:=))
 import Data.Tuple (Tuple(..))
 import Effect.Class (liftEffect)
 import Effect.Console (log)
 import Foreign.Object as Object
-import Prelude (bind, discard, pure, show, ($), (<>))
+import Prelude (bind, const, discard, pure, show, ($), (<>))
 import Pux (EffModel, noEffects)
 import Pux.DOM.Events (DOMEvent, targetValue)
 import Share.State (State)
-import Simple.JSON (writeJSON)
+import Simple.JSON (readJSON, writeJSON)
 import Web.Event.Event (preventDefault)
 
 data Event
   = EmailChange DOMEvent
   | PasswordChange DOMEvent
   | SignIn DOMEvent
+  | SignInSuccess RallyToken
+
+type RallyToken =
+  { expiredAt :: String -- DateTime
+  , refreshToken :: String
+  , refreshTokenExpiredAt :: String -- DateTime
+  , token :: String
+  , userId :: String
+  }
 
 foldp :: Event -> State -> EffModel State Event
 foldp (EmailChange event) state =
@@ -40,7 +51,17 @@ foldp (SignIn event) state =
             <> headers := Object.fromFoldable [Tuple "Content-Type" "application/json"]
             <> method := Method.POST
             <> url := "https://api.rallyapp.jp/tokens")
-        liftEffect $ log $ show response.body
+        pure do
+          body <- response.body
+          token <- either (const Nothing) Just (readJSON body)
+          pure (SignInSuccess (token :: RallyToken))
+    ]
+  }
+foldp (SignInSuccess token) state =
+  { state
+  , effects:
+    [ do
+        liftEffect $ log $ show token
         pure Nothing
     ]
   }
