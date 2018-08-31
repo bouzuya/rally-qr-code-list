88ca031cf5e393f9aed8936b159fc78d42b2327f
bouzuya <m@bouzuya.net>
2018-08-31 13:30:06 +0900

    add RouteChange event

--- src/Share/Event.purs
+++ src/Share/Event.purs
@@ -16,6 +16,7 @@ import Foreign.Object as Object
 import Prelude (bind, const, discard, pure, show, ($), (<>))
 import Pux (EffModel, noEffects)
 import Pux.DOM.Events (DOMEvent, targetValue)
+import Share.Route (Route)
 import Share.State (State)
 import Simple.JSON (readJSON, writeJSON)
 import Web.Event.Event (preventDefault)
@@ -23,6 +24,7 @@ import Web.Event.Event (preventDefault)
 data Event
   = EmailChange DOMEvent
   | PasswordChange DOMEvent
+  | RouteChange Route
   | SignIn DOMEvent
   | SignInSuccess RallyToken
 
@@ -39,6 +41,7 @@ foldp (EmailChange event) state =
   noEffects $ state { email = targetValue event }
 foldp (PasswordChange event) state =
   noEffects $ state { password = targetValue event }
+foldp (RouteChange route) state = noEffects state -- TODO
 foldp (SignIn event) state =
   { state
   , effects:
