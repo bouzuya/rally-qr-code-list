56d07a569f45bef57cfc2e04bc7fdc0b9b8f9208
bouzuya <m@bouzuya.net>
2018-08-31 13:34:00 +0900

    add route

--- src/Share/Route.purs
+++ src/Share/Route.purs
@@ -1,7 +1,20 @@
 module Share.Route
   ( Route(..)
+  , route
   ) where
+
+import Share.Path (normalizePath, toPieces)
 
 data Route
   = SignIn
   | StampRallyList
+
+route :: String -> Route
+route path =
+  let
+    normalized = normalizePath path
+  in
+    case toPieces normalized of
+      [] -> SignIn
+      ["stamp_rallies"] -> StampRallyList
+      _ -> SignIn -- default
