de0932c2eff8ef596eded658b267a5304add486c
bouzuya <m@bouzuya.net>
2018-09-02 13:43:05 +0900

    add getSpotList to Request module

--- src/Share/Request.purs
+++ src/Share/Request.purs
@@ -1,7 +1,9 @@
 module Share.Request
-  ( StampRally
+  ( Spot
+  , StampRally
   , RallyToken
   , createToken
+  , getSpotList
   , getStampRallyList
   ) where
 
@@ -14,9 +16,40 @@ import Data.Nullable (Nullable, toMaybe)
 import Data.Options ((:=))
 import Data.Tuple (Tuple(..))
 import Effect.Aff (Aff)
+import Effect.Class (liftEffect)
+import Effect.Class.Console (log)
 import Foreign.Object as Object
-import Prelude (bind, const, map, pure, (<>))
+import Prelude (bind, const, discard, map, pure, show, (<>))
 import Simple.JSON (readJSON, writeJSON)
+
+type RallyImage =
+  { id :: String
+  , position :: Int
+  , s64 :: String -- URL
+  , s640 :: String -- URL
+  }
+
+type RallySpotList =
+  { hasNextPage :: Boolean
+  , spots :: Array RallySpotSummary
+  }
+
+type RallySpotSummary =
+  { description :: String
+  , id :: Int
+  , image :: Nullable RallyImage
+  , lat :: String -- Number
+  , lng :: String -- Number
+  , lockVersion :: Int
+  , name :: String
+  , position :: Int
+  , stampByLocation :: Boolean
+  , stampByQrCode :: Boolean
+  , stampImage :: Nullable RallyImage
+  , stampRallyId :: String
+  , tagline :: String
+  , zoom :: Int
+  }
 
 type RallyStampRallyList =
   { hasNextPage :: Boolean
@@ -30,7 +63,7 @@ type RallyStampRallySummary =
   , displayName :: String
   , displayStartDatetime :: String -- DateTime
   , expiredAt :: String -- DateTime
-  , image :: Nullable { id :: String, position :: Int, s64 :: String, s640 :: String }
+  , image :: Nullable RallyImage
   , isStandardPlan :: Boolean
   , name :: String
   , noStampedStampCardsCountLimitEnabled :: Boolean
@@ -48,6 +81,12 @@ type RallyToken =
   , refreshTokenExpiredAt :: String -- DateTime
   , token :: String
   , userId :: String
+  }
+
+type Spot =
+  { id :: Int
+  , image :: Maybe String
+  , name :: String
   }
 
 type StampRally =
@@ -68,6 +107,30 @@ createToken params = do
     body <- response.body
     token <- either (const Nothing) Just (readJSON body)
     pure token :: Maybe RallyToken
+
+getSpotList :: RallyToken -> String -> Aff (Maybe (Array Spot))
+getSpotList token stampRallyId = runMaybeT do
+  spotList <- MaybeT (getSpotList' token stampRallyId)
+  pure (map toSpot spotList.spots)
+  where
+    toSpot { id, image, name } =
+      { id, image: (map (\{ s640 } -> s640) (toMaybe image)), name }
+
+getSpotList' :: RallyToken -> String -> Aff (Maybe RallySpotList)
+getSpotList' token stampRallyId = do
+  response <-
+    fetch
+      ( headers :=
+          Object.fromFoldable
+            [ Tuple "Content-Type" "application/json"
+            , Tuple "Authorization" ("Token token=" <> token.token)]
+      <> method := Method.GET
+      <> url := ("https://api.rallyapp.jp/stamp_rallies/" <> stampRallyId <> "/spots?view_type=admin"))
+  liftEffect (log (show response.body))
+  pure do
+    body <- response.body
+    spotList <- either (const Nothing) Just (readJSON body)
+    pure spotList :: Maybe RallySpotList
 
 getStampRallyList :: RallyToken -> Aff (Maybe (Array StampRally))
 getStampRallyList token = runMaybeT do
