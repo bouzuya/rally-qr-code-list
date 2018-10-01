e327af9b26e880f1417e1e10dbf5f96cfbc87d3e
bouzuya <m@bouzuya.net>
2018-10-01 16:42:27 +0900

    fix radio button style

--- src/Share/Style/SpotList.purs
+++ src/Share/Style/SpotList.purs
@@ -2,17 +2,26 @@ module Share.Style.SpotList
   ( style
   ) where
 
-import CSS (CSS, block, display, displayNone, fontSize, fromString, li, px, ul)
+import CSS (CSS, block, display, displayNone, flex, float, floatLeft, fontSize, fromString, label, li, padding, px, ul)
 import CSS.ListStyle.Type (listStyleType)
 import CSS.ListStyle.Type as ListStyleType
 import Prelude (discard, (#))
 import Share.Style.Selector ((?>))
 
+selectorStyle :: CSS
+selectorStyle = do
+  display flex
+  fontSize (16.0 # px)
+  label ?> do
+    padding (0.0 # px) (8.0 # px) (0.0 # px) (8.0 # px)
+    fromString ".value" ?> do
+      float floatLeft
+
 style :: CSS
 style = do
   fromString ".spot-list" ?> do
-    fromString ".selector" ?> do
-      fontSize (16.0 # px)
+    fromString ".selector" ?> selectorStyle
+    fromString ".error-correction-level-selector" ?> selectorStyle
     ul ?> do
       listStyleType ListStyleType.None
       li ?> do
