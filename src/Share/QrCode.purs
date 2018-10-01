d736a0a21a7e8b38ad4a3cdeee89e0d918e9c7d3
bouzuya <m@bouzuya.net>
2018-10-01 14:46:24 +0900

    add instances

--- src/Share/QrCode.purs
+++ src/Share/QrCode.purs
@@ -3,12 +3,19 @@ module Share.QrCode
   , toDataUrl
   ) where
 
+import Control.Monad.Except (except)
 import Control.Promise (Promise)
 import Control.Promise as Promise
+import Data.Either (Either(..))
+import Data.List.NonEmpty as NonEmptyList
 import Effect (Effect)
 import Effect.Aff (Aff)
 import Effect.Class (liftEffect)
+import Foreign (F, ForeignError(..))
 import Prelude (bind)
+import Simple.JSON (class ReadForeign, class WriteForeign, readImpl, writeImpl)
+
+foreign import toDataUrlImpl :: ErrorCorrectionLevelString -> String -> Effect (Promise String)
 
 data ErrorCorrectionLevel
   = L
@@ -16,17 +23,30 @@ data ErrorCorrectionLevel
   | Q
   | H
 
-foreign import toDataUrlImpl :: String -> String -> Effect (Promise String)
+type ErrorCorrectionLevelString = String
+
+instance readForeignErrorCorrectionLevel :: ReadForeign ErrorCorrectionLevel where
+  readImpl f = do
+    o <- readImpl f :: F ErrorCorrectionLevelString
+    except case o of
+      "L" -> Right L
+      "M" -> Right M
+      "Q" -> Right Q
+      "H" -> Right H
+      _ -> Left (NonEmptyList.singleton (ForeignError "Unknown Route"))
+
+instance writeForeignErrorCorrectionLevel :: WriteForeign ErrorCorrectionLevel where
+  writeImpl level = writeImpl (toStringErrorCorrectionLevel level)
+
+toStringErrorCorrectionLevel :: ErrorCorrectionLevel -> String
+toStringErrorCorrectionLevel level =
+  case level of
+    L -> "L"
+    M -> "M"
+    Q -> "Q"
+    H -> "H"
 
 toDataUrl :: ErrorCorrectionLevel -> String -> Aff String
-toDataUrl level text =
-  let
-    level' = case level of
-      L -> "L"
-      M -> "M"
-      Q -> "Q"
-      H -> "H"
-  in
-    do
-      promise <- liftEffect (toDataUrlImpl level' text)
-      Promise.toAff promise
+toDataUrl level text = do
+  promise <- liftEffect (toDataUrlImpl (toStringErrorCorrectionLevel level) text)
+  Promise.toAff promise
